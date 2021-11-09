db28bbcffe8ee75bbb9e2f30cb18f162c6977c88
Vincent <avipami@gmail.com>
2021-11-10 00:26:29 +0100

    Timer info
    Added some timer infos

--- timer.c
+++ timer.c
@@ -1,6 +1,44 @@
 #include <avr/io.h>
 #include "timer.h"
 
+/*
+Pins 5 and 6: controlled by Timer 0 in fast PWM mode (cycle length = 256)
+Setting 	Divisor 	Frequency
+0x01 	 	1 	 	62500
+0x02  		8 	 	7812.5
+0x03  		64 	 	976.5625   <--DEFAULT
+0x04 	 	256 	 	244.140625
+0x05 	 	1024 	 	61.03515625
+
+TCCR0B = (TCCR0B & 0b11111000) | <setting>;
+
+Pins 9 and 10: controlled by timer 1 in phase-correct PWM mode (cycle length = 510)
+Setting 	Divisor 	Frequency
+0x01 	 	1 	 	31372.55
+0x02 	 	8 	 	3921.16
+0x03  		64 	 	490.20   <--DEFAULT
+0x04  		256 	 	122.55
+0x05 	 	1024 	 	30.64
+
+TCCR1B = (TCCR1B & 0b11111000) | <setting>;
+
+Pins 11 and 3: controlled by timer 2 in phase-correct PWM mode (cycle length = 510)
+Setting 	Divisor 	Frequency
+0x01 	 	1  		31372.55
+0x02 	 	8 	 	3921.16
+0x03  		32  		980.39
+0x04 	 	64 	 	490.20   <--DEFAULT
+0x05 	 	128  		245.10
+0x06  		256  		122.55
+0x07 	 	1024  		30.64
+
+TCCR2B = (TCCR2B & 0b11111000) | <setting>;
+
+All frequencies are in Hz and assume a 16000000 Hz system clock.
+
+
+*/
+
 void timer_init() {
     //Timer 0
     TCCR0A |= (1 << WGM01) | (1 << WGM00);
--- timer.h
+++ timer.h
@@ -1,6 +1,6 @@
-#ifndef _TIMER_H_
-#define _TIMER_H_
+#ifndef _TIMER_H
+#define _TIMER_H
 
 void timer_init(void);
 
-#endif // _TIMER_H_
+#endif 
